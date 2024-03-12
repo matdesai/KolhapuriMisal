9b6492c32c333950aad4b93a3ffcd955223f4d7d
matinDesai <matindesai7774@gmail.com>
2024-03-12 18:16:27 +0530

    first commit in the March

---
+++ src/mat1/m1.java
@@ -0,0 +1,21 @@
+package mat1;
+
+public class m1 
+{
+	
+	public static void main(String[] args) {
+		
+		
+		String name="matindesai";
+		
+		
+	StringBuilder bf= new StringBuilder();
+	
+	bf.append(name);
+	bf.reverse();
+	
+	System.out.println(bf);
+	
+	}
+
+}
